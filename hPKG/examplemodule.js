e458d766f0bf99cea4f5469a74813e29c56dde41
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-19 21:08:35 +0300

    Slight change.. again

--- hPKG/examplemodule.js
+++ hPKG/examplemodule.js
@@ -1,4 +1,6 @@
-window.examplemodule_returnExample = async function () {
+window.examplemodule = {
+  async returnExample() {
     addLine("# This is an example module!");
     addLine("[color=black][bg=white]This text has inverted colors[/color][/bg]");
-};
+  }
+}
