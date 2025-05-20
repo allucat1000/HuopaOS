0d1c5145725a51e0a57032ff97d0d5611612beea
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-20 23:10:59 +0300

    Add files via upload

--- packages/huopacli.js
+++ packages/huopacli.js
@@ -1,34 +1,28 @@
 window.huopaCLI = {
-  initialized: false,
-  async init() {
-    await new Promise(resolve => setTimeout(resolve, 500));
-    this.initialized = true;
-    addLine("## [bg=blue]huopaCLI setup[/bg]")
-    addLine("Do you want to install huopaCLI as your system environment? [Y/n]")
-    inputAnswerActive = true;
-    await waitUntil(() => !inputAnswerActive);
-    if (inputAnswer.toLowerCase() === "y") {
-        addLine("[bg=purple]Installing huopaCLI...[/bg]")
-        const bootFile = `
-            (async function() {
-                addLine("# Welcome to [color=purple]huopaCLI![/color]");
-                addLine("What app do you want to load?");
-                addLine("\n");
-                addLine("**Terminal**");
-                inputAnswerActive = true;
-                await waitUntil(() => !inputAnswerActive);
-                if (inputAnswer === "Terminal") {
-                    addLine("[color=blue]Opening Terminal...[/color]");
-                }
-            })();
-            `;
-
-        localStorage.setItem("/system/env/boot.js", bootFile);
-
-    } else {
-        addLine("[bg=red]huopaCLI setup cancelled![/bg]")
+    async install() {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        addLine("## [bg=blue]huopaCLI setup[/bg]")
+        addLine("Do you want to install huopaCLI as your system environment? [Y/n]")
+        inputAnswerActive = true;
+        await waitUntil(() => !inputAnswerActive);
+        if (inputAnswer.toLowerCase() === "y") {
+            addLine("[bg=purple]Installing huopaCLI...[/bg]")
+            const bootFile = `loadPackage("/system/packages/huopacli.js")`;
+            localStorage.setItem("/system/env/boot.js", bootFile);
+            this.boot();
+        } else {
+            addLine("[bg=red]huopaCLI setup cancelled![/bg]")
+        }
+    },
+    
+    async boot() {
+        addLine("[bg=green]# HuopaCLI booting…[/bg]");
+        addLine("### What app do you want to open?")
+        addLine("Terminal")
+        inputAnswerActive = true;
+        await waitUntil(() => !inputAnswerActive);
+        if (inputAnswer.toLowerCase() === "terminal") {
+            boogMGR("termBoot");
+        }
     }
-}
 };
-
-window.huopaCLI.init();
