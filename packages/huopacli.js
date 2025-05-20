ee99d377a94ae8b0d06e2da58c1e30706abd51cb
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-20 22:57:53 +0300

    Add files via upload

--- packages/huopacli.js
+++ packages/huopacli.js
@@ -10,15 +10,17 @@ window.huopaCLI = {
     if (inputAnswer.toLowerCase() === "y") {
         addLine("[bg=purple]Installing huopaCLI...[/bg]")
         const bootFile = `
-            addLine("# Welcome to [color=purple]huopaCLI![/color]");
-            addLine("What app do you want to load?");
-            addLine("\\n");
-            addLine("**Terminal**");
-            inputAnswerActive = true;
-            await waitUntil(() => !inputAnswerActive);
-            if (inputAnswer === "Terminal") {
-                addLine("[color=blue]Opening Terminal...[/color]");
-            }
+            (async function() {
+                addLine("# Welcome to [color=purple]huopaCLI![/color]");
+                addLine("What app do you want to load?");
+                addLine("\\n");
+                addLine("**Terminal**");
+                inputAnswerActive = true;
+                await waitUntil(() => !inputAnswerActive);
+                if (inputAnswer === "Terminal") {
+                    addLine("[color=blue]Opening Terminal...[/color]");
+                }
+            })();
             `;
 
         localStorage.setItem("/system/env/boot.js", bootFile);
