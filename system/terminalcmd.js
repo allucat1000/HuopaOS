aa4b0789deb91067e1f637d4c3ae068addb4082e
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-21 23:27:49 +0300

    whoopsie

--- system/terminalcmd.js
+++ system/terminalcmd.js
@@ -1,7 +1,7 @@
 window.terminalcmd = {
   async listpkgs(){
     if (localStorage.getItem("/system/packageList.txt")) {
-      addLine(`[bg=palevioletred]Packages: ${localStorage.getItem("/system/packageList.txt").replace(" ","\, "}[/bg]`)
+      addLine(`[bg=palevioletred]Packages: ${localStorage.getItem("/system/packageList.txt").replace(" ",", "}[/bg]`)
     } else {
       addLine("[bg=red]No packages installed![/bg]")
     }
