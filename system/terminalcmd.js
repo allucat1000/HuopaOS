8c070a3694a0b243626bca1b65133e0f57079873
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-21 23:38:40 +0300

    Update terminalcmd.js

--- system/terminalcmd.js
+++ system/terminalcmd.js
@@ -1,13 +1,6 @@
 window.terminalcmd = {
   async listpkgs() {
-    const pkgsRaw = localStorage.getItem("/system/packageList.txt");
-    if (pkgsRaw) {
-      const pkgsClean = pkgsRaw
-        .trim()
-        .split(/\s+/)
-        .filter(Boolean)
-        .join(", ");
-      addLine(`[bg=palevioletred]Packages: ${pkgsClean}[/bg]`);
+      addLine(`[bg=palevioletred]Packages: ${localStorage.getItem("/system/packageList.txt")}[/bg]`);
     } else {
       addLine("[bg=red]No packages installed![/bg]");
     }
