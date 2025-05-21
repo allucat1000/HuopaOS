ba4700c31abb32bb9a33d77fb9aa4b26265ebfca
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-21 22:16:32 +0300

    Update terminalcmd.js

--- system/terminalcmd.js
+++ system/terminalcmd.js
@@ -1,8 +1,8 @@
 window.terminalcmd = {
   async listpkgs(){
-    try {
+    if (localStorage.getItem("/system/packageList.txt") {
       addLine(`[bg=palevioletred]Packages: ${localStorage.getItem("/system/packageList.txt")}[/bg]`)
-    } catch (e) {
+    else {
       addLine("[bg=red]No packages installed![/bg]")
     }
   }
