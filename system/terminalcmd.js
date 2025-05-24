8a9d465566affdbc66da79e54d2860b18895b5ee
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-24 12:38:23 +0300

    whoopsie

--- system/terminalcmd.js
+++ system/terminalcmd.js
@@ -47,4 +47,18 @@ window.terminalcmd = {
       }
 
       if (meta.type === "dir" && !recursive) {
-        addLine(`[bg=red]Cannot delete directory without -r: $
+        addLine(`[bg=red]Cannot delete directory without -r: ${path}[/bg]`);
+        return;
+      }
+
+      await internalFS.delDir(path);
+      addLine(`[bg=green]Deleted: ${path}[/bg]`);
+
+    } catch (e) {
+      if (!force) {
+        addLine(`[bg=red]Error deleting ${path}[/bg]`);
+        console.error(e);
+      }
+    }
+  }
+};
