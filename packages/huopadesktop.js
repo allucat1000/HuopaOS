97bf135f1abc9e03fab8197b127a4d9b3dec37ec
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-25 22:08:27 +0300

    Add files via upload

--- packages/huopadesktop.js
+++ packages/huopadesktop.js
@@ -1,9 +1,9 @@
 window.huopadesktop = {
     async install() {
         await new Promise(resolve => setTimeout(resolve, 100));
-        addLine("## [line=blue]HuopaDesktop setup[/line]")
-        addLine("Do you want to install HuopaDesktop? [Y/n]")
-        addLine("HuopaDesktop uses the Quantum display manager.")
+        addLine("## [line=blue]HuopaDesktop setup[/line]");
+        addLine("Do you want to install HuopaDesktop? [Y/n]");
+        addLine("HuopaDesktop uses the Quantum display manager.");
         inputAnswerActive = true;
         await waitUntil(() => !inputAnswerActive);
         if (inputAnswer.toLowerCase() === "y") {
@@ -17,9 +17,30 @@ window.huopadesktop = {
                 "path":"/system/packages/huopadesktop/quantum.js",
             }
             internalFS.createPath("/system/env/config.json", "file", bootConfig);
-            internalFS.loadPackage("/system/packages/huopadesktop/quantum")
+            internalFS.loadPackage("/system/packages/huopadesktop/quantum.js");
         } else {
-            addLine("[line=red]HuopaDesktop installation has been cancelled.[/line]")
+            addLine("[line=red]HuopaDesktop installation has been cancelled.[/line]");
+        }
+    },
+    async boot() {
+        const bootConfig = internalFS.getFile("/system/env/config.json");
+        if (bootConfig) {
+            addLine("Boot config found! Attempting to boot from specified path.");
+            if (!bootConfig.path) {
+                addLine("Incorrect boot config!");
+                addLine("Please reinstall HuopaDesktop!");
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                this.install();
+            } else {
+                loadPackage(bootConfig.path);
+                await new Promise(resolve => setTimeout(resolve, 50));
+                quantum.init()
+            }
+
+        } else {
+            addLine("HuopaDesktop isn't installed yet!");
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            this.install();
         }
     }
 };
