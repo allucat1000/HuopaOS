967af60bad8107f463b7ac5916028459497e508a
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-25 22:35:36 +0300

    Add files via upload

---
+++ modules/quantum.js
@@ -0,0 +1,35 @@
+window.quantum = {
+    initialized: false,
+    version: "0.0.1",
+    bootTime: null,
+
+    get document() {
+        return new Proxy(document, {
+            get(target, prop) {
+                if (prop === "createElement") {
+                    return (tag) => {
+                        const restrictedTags = ["script", "iframe", "object"];
+                        if (restrictedTags.includes(tag.toLowerCase())) {
+                            throw new Error(`Quantum security: <${tag}> tag is not allowed.`);
+                        }
+                        return target.createElement(tag);
+                    };
+                }
+
+                return target[prop];
+            },
+            set(target, prop, value) {
+                target[prop] = value;
+                return true;
+            }
+        });
+    },
+
+    init() {
+        if (this.initialized) return "Quantum initialized already!";
+        this.initialized = true;
+        this.bootTime = Date.now();
+        addLine("[line=cyan]Loading Quantum Display Manager...[/line]");
+        addLine(`Version: ${this.version}`)
+    }
+};
