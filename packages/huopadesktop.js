46901d76a38f07902c7f1ab45cee0037b7835510
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-05-25 20:55:52 +0300

    Update and rename huopacli.js to huopadesktop.js

---
+++ packages/huopadesktop.js
@@ -0,0 +1,21 @@
+window.huopadesktop = {
+    async install() {
+        await new Promise(resolve => setTimeout(resolve, 100));
+        addLine("## [line=blue]HuopaDesktop setup[/line]")
+        addLine("Do you want to install HuopaDesktop? [Y/n]")
+        addLine("HuopaDesktop uses the Quantum display manager.")
+        inputAnswerActive = true;
+        await waitUntil(() => !inputAnswerActive);
+        if (inputAnswer.toLowerCase() === "y") {
+            addLine("[line=blue]Installing HuopaDesktop...[/line]")
+            internalFS.downloadPackage("huopadesktop/quantum.js")
+            const bootConfig = {
+                "path":"/system/env/quantum.js",
+            }
+            internalFS.createPath("/system/env/config.json", "file", bootConfig);
+            internalFS.loadPackage("/system/packages/huopadesktop/quantum.js")
+        } else {
+            addLine("[line=red]HuopaDesktop installation has been cancelled.[/line]")
+        }
+    }
+};
