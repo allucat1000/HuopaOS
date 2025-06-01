cd68cbb8425d8a9834331145ba20154f14facf98
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-06-01 13:34:28 +0300

    Update terminalcmd.js

--- system/terminalcmd.js
+++ system/terminalcmd.js
@@ -5,9 +5,9 @@ window.terminalcmd = {
     const packagesString = cleanedPackages.join(" ");
 
     if (packagesString) {
-      addLine(`[line=palevioletred]Packages: ${packagesString}[/line]`);
+      sys.addLine(`[line=palevioletred]Packages: ${packagesString}[/line]`);
     } else {
-      addLine("[line=red]No packages installed![/line]");
+      sys.addLine("[line=red]No packages installed![/line]");
     }
   },
 
@@ -17,7 +17,7 @@ window.terminalcmd = {
 
   async rm(args) {
     if (!args || args.length === 0) {
-      addLine("Usage: rm [-rf] <path>");
+      sys.addLine("Usage: rm [-rf] <path>");
       return;
     }
   
@@ -35,33 +35,33 @@ window.terminalcmd = {
     }
   
     if (!path) {
-      addLine("Usage: rm [-rf] <path>");
+      sys.addLine("Usage: rm [-rf] <path>");
       return;
     }
   
     if (path === "/" && !force) {
-      addLine("[line=red]Refusing to delete / unless -f is provided.[/line]");
+      sys.addLine("[line=red]Refusing to delete / unless -f is provided.[/line]");
       return;
     }
   
     try {
       const meta = internalFS.getMeta(path);
       if (!meta) {
-        if (!force) addLine(`[line=red]File not found: ${path}[/line]`);
+        if (!force) sys.addLine(`[line=red]File not found: ${path}[/line]`);
         return;
       }
   
       if (meta.type === "dir" && !recursive) {
-        addLine(`[line=red]Cannot delete directory without -r: ${path}[/line]`);
+        sys.addLine(`[line=red]Cannot delete directory without -r: ${path}[/line]`);
         return;
       }
   
       await internalFS.delDir(path, new Set(), recursive, force)
-      addLine(`[line=green]Deleted: ${path}[/line]`);
+      sys.addLine(`[line=green]Deleted: ${path}[/line]`);
   
     } catch (e) {
       if (!force) {
-        addLine(`[line=red]Error deleting ${path}[/line]`);
+        sys.addLine(`[line=red]Error deleting ${path}[/line]`);
         console.error(e);
       }
     }
@@ -72,7 +72,7 @@ async ls(args) {
 
   for (const arg of args || []) {
     if (arg.startsWith("-")) {
-      addLine("No args available for ls currently, sorry!");
+      sys.addLine("No args available for ls currently, sorry!");
     } else {
       path = arg;
     }
@@ -81,40 +81,40 @@ async ls(args) {
   try {
     const meta = internalFS.getMeta(path);
     if (!meta) {
-      addLine(`[line=red]File not found: ${path}[/line]`);
+      sys.addLine(`[line=red]File not found: ${path}[/line]`);
       return;
     }
 
 
     const fileArray = JSON.parse(internalFS.getFile(path) || "[]");
     const fileList = fileArray.map(f => f.replace(`${path}/`, "")).join('\n');
-    addLine(`All files in directory: ${path}`);
-    addLine(fileList);
+    sys.addLine(`All files in directory: ${path}`);
+    sys.addLine(fileList);
     
     } catch (e) {
       if (e.message.includes("is not valid JSON")) {
-        addLine(`[line=red]You cannot run ls on a file![/line]`);
+        sys.addLine(`[line=red]You cannot run ls on a file![/line]`);
       } else {
-        addLine(`[line=red]Unknown error occurred: ${e}[/line]`);
+        sys.addLine(`[line=red]Unknown error occurred: ${e}[/line]`);
       }
     }
   },
   
   async help() {
-    addLine("# Terminal commands");
+    sys.addLine("# Terminal commands");
     
-    addLine("* rm");
-    addLine("> Delete a directory / file. Usage: rm [options] <path>");
-    addLine("> Options: -r (recursive), -f (force)");
+    sys.addLine("* rm");
+    sys.addLine("> Delete a directory / file. Usage: rm [options] <path>");
+    sys.addLine("> Options: -r (recursive), -f (force)");
     
-    addLine("* listpkgs");
-    addLine("> View your installed packages.");
+    sys.addLine("* listpkgs");
+    sys.addLine("> View your installed packages.");
     
-    addLine("* clear");
-    addLine("> Clear the terminal.");
+    sys.addLine("* clear");
+    sys.addLine("> Clear the terminal.");
     
-    addLine("* ls");
-    addLine('> List files and directories in the specified path (default: "/system"). Usage: ls <path>');
+    sys.addLine("* ls");
+    sys.addLine('> List files and directories in the specified path (default: "/system"). Usage: ls <path>');
   }
 
 };
