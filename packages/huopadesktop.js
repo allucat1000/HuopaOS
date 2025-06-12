80b70d441e86ece36d45d07488dc628812a1dbfe
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-06-12 14:37:05 +0300

    Bump to 0.5.7 (HuopaDesktop)

--- packages/huopadesktop.js
+++ packages/huopadesktop.js
@@ -3,7 +3,7 @@ window.huopadesktop = (() => {
     let sysTempInfo = {
         "startMenuOpen":false
     }
-    const version = "0.5.6";
+    const version = "0.5.7";
     // Priv Sys Funcs
     const mainInstaller = async () => {
         try {
