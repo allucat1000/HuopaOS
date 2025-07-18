770f4902efdc14986068be5ab4f51e1ff0883e1b
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-07-18 16:27:34 +0300

    Whoops, fix!

--- packages/huopadesktop.js
+++ packages/huopadesktop.js
@@ -573,7 +573,7 @@ window.huopadesktop = (() => {
                         if (document.body) return resolve();
                         window.addEventListener("DOMContentLoaded", () => resolve(), { once: true });
                         });
-                        document.body.style.height = "calc(100vh - 20px)";
+                        document.body.style.height = "calc(100vh - 35px)";
                         document.body.append(systemStyles);
                         await eval("(async () => {" + ${JSON.stringify(appCode)} + "})()");
                     } catch (e) {
