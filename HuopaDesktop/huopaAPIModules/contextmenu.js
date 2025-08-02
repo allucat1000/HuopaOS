d5cf2f7a3b47f8c381963f1d99e8527d356604bf
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-08-02 17:18:59 +0300

    Small theming change to right click menu

--- HuopaDesktop/huopaAPIModules/contextmenu.js
+++ HuopaDesktop/huopaAPIModules/contextmenu.js
@@ -6,7 +6,7 @@ return {
             e.preventDefault();
             if (contextMenu) contextMenu.remove();
             contextMenu = document.createElement("div");
-            contextMenu.style = "background-color: rgba(55, 55, 55, 0.65); border-radius: 0.5em; border: rgba(105, 105, 105, 0.65) 1px solid; position: absolute; z-index: 9999; box-shadow: 2px 2px 10px rgba(0,0,0,0.1); display: block; padding: 0;";
+            contextMenu.style = "background-color: rgba(45, 45, 45, 0.8); border-radius: 0.5em; border: rgba(105, 105, 105, 0.65) 1px solid; position: absolute; z-index: 9999; box-shadow: 2px 2px 10px rgba(0,0,0,0.1); display: block; padding: 0;";
             contextMenu.style.top = `${e.clientY}px`;
             contextMenu.style.left = `${e.clientX}px`;
             for (const opt of options) {
