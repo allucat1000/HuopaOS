9340fbb20ae92c9ca5159618528310ac73b2ccab
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-08-06 12:10:40 +0300

    Fixed Goose Cam resolution

--- HuopaDesktop/AppStore/GooseCam/main.js
+++ HuopaDesktop/AppStore/GooseCam/main.js
@@ -4,13 +4,13 @@ await setAttrs(img, {
     "style":"width: 100%; height: 100%;"
 });
 document.body.append(img)
-huopaAPI.setMaxWindowSize(640, 360);
-huopaAPI.setMinWindowSize(640, 360);
+huopaAPI.setMaxWindowSize(640, 480);
+huopaAPI.setMinWindowSize(640, 480);
 huopaAPI.setWindowConfig("ignoreTile");
 huopaAPI.removeTitlebar();
 async function setWinPos() {
     huopaAPI.setWindowPosition("0", "0");
-    huopaAPI.setWindowSize("640px", "360px");
+    huopaAPI.setWindowSize("640px", "480px");
     setTimeout(setWinPos, 250)
 }
 setWinPos()
