28605c7887f94c374a3bdbfa9820ac325e2ce523
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-08-06 12:27:24 +0300

    Made GooseCam more customizable and changed Blue Theme style a bit

--- HuopaDesktop/AppStore/GooseCam/main.js
+++ HuopaDesktop/AppStore/GooseCam/main.js
@@ -3,14 +3,20 @@ await setAttrs(img, {
     "src":"https://goose.izkuipers.nl/feed",
     "style":"width: 100%; height: 100%;"
 });
+let sizeConfig = await huopaAPI.applicationStorageRead("config.json");
+if (!sizeConfig) {
+    sizeConfig = '{"sizemult":0.5}';
+    await huopaAPI.applicationStorageWrite("config.json", "file", sizeConfig);
+}
+sizeConfig = JSON.parse(sizeConfig);
 document.body.append(img)
-huopaAPI.setMaxWindowSize(640, 480);
-huopaAPI.setMinWindowSize(640, 480);
+huopaAPI.setMaxWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
+huopaAPI.setMinWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
 huopaAPI.setWindowConfig("ignoreTile");
 huopaAPI.removeTitlebar();
 async function setWinPos() {
     huopaAPI.setWindowPosition("0", "0");
-    huopaAPI.setWindowSize("640px", "480px");
+    huopaAPI.setWindowSize(640 * sizeConfig.sizemult + "px", 480 * sizeConfig.sizemult + "px");
     setTimeout(setWinPos, 250)
 }
 setWinPos()
