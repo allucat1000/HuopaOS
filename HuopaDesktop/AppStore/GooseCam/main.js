6873d45364f6cdcd3edf1a871ccacd65aa641bf6
Allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-08-06 13:20:19 +0300

    Added config option for forcing the Goose Cam window position to the corner

--- HuopaDesktop/AppStore/GooseCam/main.js
+++ HuopaDesktop/AppStore/GooseCam/main.js
@@ -5,18 +5,20 @@ await setAttrs(img, {
 });
 let sizeConfig = await huopaAPI.applicationStorageRead("config.json");
 if (!sizeConfig) {
-    sizeConfig = '{"sizemult":0.5}';
+    sizeConfig = '{"sizemult":0.5, "forcepos":true}';
     await huopaAPI.applicationStorageWrite("config.json", "file", sizeConfig);
 }
 sizeConfig = JSON.parse(sizeConfig);
 document.body.append(img)
-huopaAPI.setMaxWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
-huopaAPI.setMinWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
-huopaAPI.setWindowConfig("ignoreTile");
-huopaAPI.removeTitlebar();
-async function setWinPos() {
-    huopaAPI.setWindowPosition("0", "0");
-    huopaAPI.setWindowSize(640 * sizeConfig.sizemult + "px", 480 * sizeConfig.sizemult + "px");
-    setTimeout(setWinPos, 250)
+if (sizeConfig.forcepos) {
+    huopaAPI.setMaxWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
+    huopaAPI.setMinWindowSize(640 * sizeConfig.sizemult, 480 * sizeConfig.sizemult);
+    huopaAPI.setWindowConfig("ignoreTile");
+    huopaAPI.removeTitlebar();
+    async function setWinPos() {
+        huopaAPI.setWindowPosition("0", "0");
+        huopaAPI.setWindowSize(640 * sizeConfig.sizemult + "px", 480 * sizeConfig.sizemult + "px");
+        setTimeout(setWinPos, 250)
+    }
+    setWinPos();
 }
-setWinPos()
